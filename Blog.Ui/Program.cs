ef130de52e43c879a4d4c81b67e4df20a4bd22d3
necil cakmak <necilcakmak@hotmail.com>
2022-06-27 15:10:14 +0300

    Blazor Client Add

---
+++ Blog.Ui/Program.cs
@@ -0,0 +1,15 @@
+using Blazored.Modal;
+using Blog.Ui;
+using Blog.Ui.Utils;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredModal();
+builder.Services.AddScoped<ModalManager>();
+await builder.Build().RunAsync();
